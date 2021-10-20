78fa38ddb04a42ab13254e00ea7734a351ea494d
Neha Sontakke <wasnik.neha@gmail.com>
2021-10-20 19:22:29 +0530

    added the pagination example

---
+++ demo4/src/App.js
@@ -0,0 +1,42 @@
+
+import axios from "axios";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination.js";
+import React, { useEffect, useState } from "react";
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary-mb-3">List of Articles</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
+    </div>
+  );
+}
+export default App;
