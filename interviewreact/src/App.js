63c29351ed4e8ca07024e6e944e754a47b2b81d2
Neha Sontakke <wasnik.neha@gmail.com>
2022-01-01 17:28:32 +0530

    print the text

---
+++ interviewreact/src/App.js
@@ -0,0 +1,23 @@
+//Following was one of the simple questions ask to freshers in react interview , the question is get 
+//Que: Write a code to print the text as soon as user starts typing in the text box. 
+
+
+import React, { useState }  from 'react';
+import './App.css';
+
+function App() {
+ const [type , setType] = useState("");
+
+ const handleChange = (e)=>{
+   setType(e.target.value);
+   console.log(type);
+ }
+
+  return (
+    <div className="App">
+      <input type="text" value={type} onChange={handleChange}/>
+    </div>
+  );
+}
+
+export default App;
