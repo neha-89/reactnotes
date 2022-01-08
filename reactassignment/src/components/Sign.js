2b05a0499f62f800aa0f2dbe2d6d91a835df0495
Neha Sontakke <wasnik.neha@gmail.com>
2022-01-08 23:22:26 +0530

    store data in localstorage

---
+++ reactassignment/src/components/Sign.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Sign =()=>{
+    return(
+        <div>
+        <input type="text" name={} value={}  />
+        <input type="password" name={} value={}  />
+        <input type="email" name={} value={}  />
+        <input type="tel" name={} value={}  />
+        
+        </div>
+    )
+}
+
+export default Sign;
