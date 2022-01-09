544f8a98b2165f8e3559428d84cdd3c59bc75070
Neha Sontakke <wasnik.neha@gmail.com>
2022-01-09 23:19:27 +0530

    updated code

--- interviewreact1/src/App.js
+++ interviewreact1/src/App.js
@@ -1,65 +1,26 @@
 // write a code to create a toggle button when the user clicks on the button image1 should appear and when clicks on the 
 // that same button image2 should appear and the image1 gets hidden.
 import React, { useState } from 'react';
+
 import './App.css';
 
 
-const data = [
-  {
-    id: "1",
-    key: "1",
-    title: "Title1",
-    text: "Text1.",
-    img: "1.jpg"
-  },
-  {
-    id: "2",
-    key: "2",
-    title: "Title2",
-    text: "Text2.",
-    img: "2.jpg"
-  }
-]
-
 function App() {
 
-  const white = require('./images/white.png')
-const black = require('./images/black.png')
-const shirts = { white, black }
-//Now bind the selected T-Shirt to a state's property and pass it as src:
-
-const Shirts = () =>{
-    const [selected, setSelected] = useState(shirts.white)
-
-   return(
-       <>
-           <img src={selected} alt='shirt' />
-           <button onClick={() => setSelected(shirts.black)}> Click</button>
-       </>
-   )
+const [show,setShow] = useState(true);
+function handleClick(){
+  setShow(false);
 }
-//   const [display , setDisplay] = useState(true);
-//   const [hide,setHide] = useState(false);
 
-// const handleClick = ()=>{
-// }
-//   return (
-//     <div className="App">
-//      <img src=""/>
-    
-//      <button onClick={handleClick}>Toggle</button>
-//     </div>
-//   );
-   
+  return(
+    <div>
+     
+  {show?<img src="https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt ="cat"/>:<img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg" alt="cat1"/>} 
+
+  <button onClick={handleClick}>Toggle</button>
+    </div>
+  )
 }
-var MyIconButton= React.createClass({
-  render: function(){
-      var pic= this.props.pressed? this.props.onpic : this.props.offpic
-      return <img 
-          src={pic} 
-          onClick={this.props.tuggleSelection}  //updateFilter is wierd name
-      />
-  }
-})
 
 export default App;
+
